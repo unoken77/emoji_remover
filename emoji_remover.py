07f3ba430da776f7d609dc110b29357362a1617c
Kenichi <newunkn@gmail.com>
2020-03-03 23:51:44 +0900

    bug fixed

--- emoji_remover.py
+++ emoji_remover.py
@@ -2,18 +2,23 @@ import glob
 import os
 import sys
 
-# 拡張子.txtのファイルを取得する
-path_text = './dir/*txt'
-i = 1
- 
-# txtファイルを取得する
-flist = glob.glob(path_text)
+while True:
+    # 指定したディレクトリのファイルを取得する
+    path_text = input("BMP外の文字を除外したいディレクトリを指定してください")
 
-# BMP外を''に置換するマップ
-non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), '_')
+    if path_text[-1] == "\\":
+        print('true')
+        path_text=path_text.rstrip('\\')
 
-# ファイル名を一括で変更する
-for file in flist:
-    new_file_name=file.translate(non_bmp_map)
-    os.rename(file ,new_file_name)
-    i+=1
+    flist = glob.glob(path_text+"/*")
+
+    # BMP外を''に置換するマップ
+    non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), '_')
+
+    # ファイル名を一括で変更する
+    for file in flist:
+        #print(file)
+        new_file_name=file.translate(non_bmp_map)
+        os.rename(file ,new_file_name)
+
+    print("renamed")
