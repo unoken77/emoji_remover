41959ae531c0e3c81a5953f2cb5fd25c2a026823
Kenichi <newunkn@gmail.com>
2020-03-06 10:52:43 +0900

    fixed to use emoji library

--- emoji_remover.py
+++ emoji_remover.py
@@ -1,26 +1,29 @@
 import glob
 import os
 import sys
+import emoji
+
+def remove_emoji(src_str):
+    return ''.join(c for c in src_str if c not in emoji.UNICODE_EMOJI)
 
 while True:
     # 指定したディレクトリのファイルを取得する
-    path_text = input("BMP外の文字を除外したいディレクトリを指定してください")
+    path_text = input("絵文字を除外したいディレクトリを指定してください")
 
     if path_text[-1] == "\\":
-        print('true')
         path_text=path_text.rstrip('\\')
 
     flist = glob.glob(path_text+"/*")
 
-    # BMP外を''に置換するマップ
-    non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), '_')
-
     # ファイル名を一括で変更する
+    file_index=1
     for file in flist:
-        #print(file)
         old_file_name=file
-        new_file_name=file.translate(non_bmp_map)
+        new_file_name=remove_emoji(file)
         os.rename(file ,new_file_name)
         if old_file_name != new_file_name:
-            print('変更後:'+new_file_name)
-    print("renamed")
+            new_file_name_e=new_file_name.encode('cp932', 'ignore')
+            new_file_name_d=new_file_name_e.decode('cp932')
+            print(new_file_name+'に変更しました')
+        file_index = file_index + 1
+    print("完了")
